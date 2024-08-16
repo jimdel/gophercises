29c350a875e40cf16e4a87726587ded50e24ee4a
James DeLay <jdelay.jr@gmail.com>
2024-08-16 00:03:13 -0400

    add: parse flags

--- e3-TextAdventure/main.go
+++ e3-TextAdventure/main.go
@@ -11,6 +11,7 @@ func main() {
 
 	storyPath := flag.String("story", "./gopher.json", "path to story file JSON")
 	startWith := flag.String("startwith", Intro, "name of story to start the adventure with")
+	flag.Parse()
 	cyoa := jsonParser.ReadAndParse(*storyPath)
 
 	web.StartServer(cyoa, *startWith)
